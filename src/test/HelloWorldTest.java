69055c0bcfaa42d40853b82fb2681da8a32f4e9b
ArditPeni <arditpeni@gmail.com>
2021-10-04 19:04:43 -0400

    updating hello worrld test

--- src/test/HelloWorldTest.java
+++ src/test/HelloWorldTest.java
@@ -4,5 +4,7 @@ public class HelloWorldTest {
     public static void main(String[] args) {
 
         System.out.println("hello world");
+
+        System.out.println("one more hell oworrld");
     }
 }
